196751e94cc7b92eda9a8a759c96ce839d6352ae
fly to the stars <1242119064@qq.com>
2022-09-07 11:00:48 +0800

    Revert "删除文件 LANChat_APP_Release/release"
    This reverts commit 40485674107a0e50e5c94c231bc22f66acbd4e7d.

---
+++ LANChat_APP_Release/release/moc_ChatWidget.cpp
@@ -0,0 +1,196 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'ChatWidget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../ChatWidget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'ChatWidget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_ChatWidget_t {
+    QByteArrayData data[24];
+    char stringdata0[466];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ChatWidget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ChatWidget_t qt_meta_stringdata_ChatWidget = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "ChatWidget"
+QT_MOC_LITERAL(1, 11, 16), // "Socketerror_slot"
+QT_MOC_LITERAL(2, 28, 0), // ""
+QT_MOC_LITERAL(3, 29, 28), // "QAbstractSocket::SocketError"
+QT_MOC_LITERAL(4, 58, 11), // "socketError"
+QT_MOC_LITERAL(5, 70, 20), // "SocketReadyRead_slot"
+QT_MOC_LITERAL(6, 91, 21), // "on_sendButton_clicked"
+QT_MOC_LITERAL(7, 113, 21), // "on_exitButton_clicked"
+QT_MOC_LITERAL(8, 135, 17), // "sendFileName_slot"
+QT_MOC_LITERAL(9, 153, 8), // "filename"
+QT_MOC_LITERAL(10, 162, 26), // "on_sendFileToolBtn_clicked"
+QT_MOC_LITERAL(11, 189, 34), // "on_fontComboBox_currentFontCh..."
+QT_MOC_LITERAL(12, 224, 1), // "f"
+QT_MOC_LITERAL(13, 226, 35), // "on_sizeComboBox_currentIndexC..."
+QT_MOC_LITERAL(14, 262, 4), // "arg1"
+QT_MOC_LITERAL(15, 267, 22), // "on_boldToolBtn_clicked"
+QT_MOC_LITERAL(16, 290, 7), // "checked"
+QT_MOC_LITERAL(17, 298, 24), // "on_italicToolBtn_clicked"
+QT_MOC_LITERAL(18, 323, 27), // "on_underlineToolBtn_clicked"
+QT_MOC_LITERAL(19, 351, 23), // "on_colorToolBtn_clicked"
+QT_MOC_LITERAL(20, 375, 43), // "on_messageTextEdit_currentCha..."
+QT_MOC_LITERAL(21, 419, 15), // "QTextCharFormat"
+QT_MOC_LITERAL(22, 435, 6), // "format"
+QT_MOC_LITERAL(23, 442, 23) // "on_clearToolBtn_clicked"
+
+    },
+    "ChatWidget\0Socketerror_slot\0\0"
+    "QAbstractSocket::SocketError\0socketError\0"
+    "SocketReadyRead_slot\0on_sendButton_clicked\0"
+    "on_exitButton_clicked\0sendFileName_slot\0"
+    "filename\0on_sendFileToolBtn_clicked\0"
+    "on_fontComboBox_currentFontChanged\0f\0"
+    "on_sizeComboBox_currentIndexChanged\0"
+    "arg1\0on_boldToolBtn_clicked\0checked\0"
+    "on_italicToolBtn_clicked\0"
+    "on_underlineToolBtn_clicked\0"
+    "on_colorToolBtn_clicked\0"
+    "on_messageTextEdit_currentCharFormatChanged\0"
+    "QTextCharFormat\0format\0on_clearToolBtn_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ChatWidget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      14,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   84,    2, 0x08 /* Private */,
+       5,    0,   87,    2, 0x08 /* Private */,
+       6,    0,   88,    2, 0x08 /* Private */,
+       7,    0,   89,    2, 0x08 /* Private */,
+       8,    1,   90,    2, 0x08 /* Private */,
+      10,    0,   93,    2, 0x08 /* Private */,
+      11,    1,   94,    2, 0x08 /* Private */,
+      13,    1,   97,    2, 0x08 /* Private */,
+      15,    1,  100,    2, 0x08 /* Private */,
+      17,    1,  103,    2, 0x08 /* Private */,
+      18,    1,  106,    2, 0x08 /* Private */,
+      19,    0,  109,    2, 0x08 /* Private */,
+      20,    1,  110,    2, 0x08 /* Private */,
+      23,    0,  113,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    9,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QFont,   12,
+    QMetaType::Void, QMetaType::QString,   14,
+    QMetaType::Void, QMetaType::Bool,   16,
+    QMetaType::Void, QMetaType::Bool,   16,
+    QMetaType::Void, QMetaType::Bool,   16,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 21,   22,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void ChatWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        ChatWidget *_t = static_cast<ChatWidget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->Socketerror_slot((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
+        case 1: _t->SocketReadyRead_slot(); break;
+        case 2: _t->on_sendButton_clicked(); break;
+        case 3: _t->on_exitButton_clicked(); break;
+        case 4: _t->sendFileName_slot((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 5: _t->on_sendFileToolBtn_clicked(); break;
+        case 6: _t->on_fontComboBox_currentFontChanged((*reinterpret_cast< const QFont(*)>(_a[1]))); break;
+        case 7: _t->on_sizeComboBox_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 8: _t->on_boldToolBtn_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 9: _t->on_italicToolBtn_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 10: _t->on_underlineToolBtn_clicked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 11: _t->on_colorToolBtn_clicked(); break;
+        case 12: _t->on_messageTextEdit_currentCharFormatChanged((*reinterpret_cast< const QTextCharFormat(*)>(_a[1]))); break;
+        case 13: _t->on_clearToolBtn_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractSocket::SocketError >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject ChatWidget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_ChatWidget.data,
+      qt_meta_data_ChatWidget,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *ChatWidget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ChatWidget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ChatWidget.stringdata0))
+        return static_cast<void*>(const_cast< ChatWidget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int ChatWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 14)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 14;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 14)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 14;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
